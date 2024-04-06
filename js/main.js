9eb6e6e11fa85b8bf4f8d14e516248d107be5279
Touko Päivänsalo <touko.paivansalo@gmail.com>
2024-04-06 16:28:24 +0300

    Add mock gradient mask

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import '../style.css'
 
 const dot = document.getElementById("dot");
-const maskCanvas = document.getElementById("dot");
-const ctx = maskCanvas.getContext("2d");
+const maskCanvas = document.getElementById("maskCanvas");
+const rect = maskCanvas.getBoundingClientRect();
 
-let x, y;
+let lookX = 0, lookY = 0;
 
 /*
 webgazer.setGazeListener((data, elapsedTime) => {
@@ -16,28 +16,55 @@ webgazer.setGazeListener((data, elapsedTime) => {
   y = data.y;
   //dot.style.top = `${data.y}px`;
   //dot.style.left = `${data.x}px`;
+  ctx.fillRect(0, 0, maskCanvas.width, maskCanvas.height);
 }).begin();
 */
 
 document.addEventListener("mousemove", event => {
-  x = event.pageX;
-  y = event.pageY;
+  lookX = event.pageX;
+  lookY = event.pageY;
 })
 
-webgazer.begin();
+//webgazer.begin();
 
-const loop = () => {
-  dot.style.top = `${y}px`;
-  dot.style.left = `${x}px`;
+const drawGradient = (ctx, x, y) => {
+  const radius = 20;
+  const gradient = ctx.createRadialGradient(x, y, 1, x, y, radius);
+  gradient.addColorStop(0, '#fff2');
+  gradient.addColorStop(0.3, '#fff1');
+  gradient.addColorStop(1, '#fff0');
 
-  const gradient = ctx.createRadialGradient(x, y, 10, x, y, 100);
-  gradient.addColorStop(0, '#000f');
-  gradient.addColorStop(1, '#0000');
-  ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fillStyle = gradient;
+  ctx.fillRect(x - radius, y - radius, radius * 2, radius * 2);
   ctx.fill();
-
-  requestAnimationFrame(loop);
 }
 
-requestAnimationFrame(loop);
+let circles = [];
+
+const loop = () => {
+  dot.style.top = `${lookY}px`;
+  dot.style.left = `${lookX}px`;
+
+  const ctx = maskCanvas.getContext("2d");
+  ctx.clearRect(0, 0, maskCanvas.width, maskCanvas.height);
+
+  const x = (lookX - rect.left) * maskCanvas.width / rect.width;
+  const y = (lookY - rect.top) * maskCanvas.height / rect.height;
+
+  if (circles.length > 100) {
+    circles.shift();
+  }
+  circles.push({ x, y });
+
+  let i = 0;
+  for (let circle of circles) {
+    console.log(i);
+    i += 1;
+    drawGradient(ctx, circle.x, circle.y);
+  }
+  drawGradient(ctx, x, y);
+
+  setTimeout(loop, 50);
+}
+
+loop();
