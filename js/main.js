9ec0d598a86e5dcd16bb1154e3c1383a274388b5
Touko Päivänsalo <touko.paivansalo@gmail.com>
2024-04-06 15:41:36 +0300

    Merge

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 import '../style.css'
 
+const dot = document.getElementById("dot");
+const maskCanvas = document.getElementById("dot");
+const ctx = maskCanvas.getContext("2d");
+
+let x, y;
+
+/*
 webgazer.setGazeListener((data, elapsedTime) => {
+  console.log(data); //elapsed time is based on time since begin was called
   if (data == null) {
     return;
   }
-  var xprediction = data.x; //these x coordinates are relative to the viewport
-  var yprediction = data.y; //these y coordinates are relative to the viewport
-  console.log(elapsedTime); //elapsed time is based on time since begin was called
+  x = data.x;
+  y = data.y;
+  //dot.style.top = `${data.y}px`;
+  //dot.style.left = `${data.x}px`;
 }).begin();
+*/
 
+document.addEventListener("mousemove", event => {
+  x = event.pageX;
+  y = event.pageY;
+})
 
-function getPrediction() {
-  var prediction = webgazer.getCurrentPrediction();
-  if (prediction) {
-    var x = prediction.x;
-    var y = prediction.y;
-  }
+webgazer.begin();
+
+const loop = () => {
+  dot.style.top = `${y}px`;
+  dot.style.left = `${x}px`;
+
+  const gradient = ctx.createRadialGradient(x, y, 10, x, y, 100);
+  gradient.addColorStop(0, '#000f');
+  gradient.addColorStop(1, '#0000');
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = gradient;
+  ctx.fill();
+
+  requestAnimationFrame(loop);
 }
+
+requestAnimationFrame(loop);
